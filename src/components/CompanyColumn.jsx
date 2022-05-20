bfb3f18f18540a5fece13370aa328634e9c0110b
Reza <rezamajidi.1992@gmail.com>
2022-05-20 23:47:13 +0430

    feat: add scroll for time slot lists

--- src/components/CompanyColumn.jsx
+++ src/components/CompanyColumn.jsx
@@ -17,21 +17,24 @@ const CompanyColumn = ({ id, name, type, timeSlots }) => {
         <TimeSlot start={"2022-05-05"} end={"2022-06-06"} />
       </div>
       <p className="mt-10 mb-2 text-sm text-gray-700">Available time slots</p>
-      <div>
-        {Object.keys(groupTimesByEndDate(timeSlots)).map((key, index) => (
-          <div className="mb-1" key={index}>
-            <p className="p-4 mb-2 text-xl text-center text-gray-800 border border-indigo-100 rounded-sm bg-indigo-50">
-              {format(new Date(key), "ccc, MMM do")}
-            </p>
-            <ul>
-              {groupTimesByEndDate(timeSlots)[key].map((date, index) => (
-                <li className="mb-2" key={index}>
-                  <TimeSlot start={date.start_time} end={date.end_time} />
-                </li>
-              ))}
-            </ul>
-          </div>
-        ))}
+      <div className="relative overflow-hiden h-96">
+        <div className="h-full pb-12 overflow-y-auto">
+          {Object.keys(groupTimesByEndDate(timeSlots)).map((key, index) => (
+            <div className="mb-1" key={index}>
+              <p className="p-4 mb-2 text-xl text-center text-gray-800 border border-indigo-100 rounded-sm bg-indigo-50">
+                {format(new Date(key), "ccc, MMM do")}
+              </p>
+              <ul>
+                {groupTimesByEndDate(timeSlots)[key].map((date, index) => (
+                  <li className="mb-2" key={index}>
+                    <TimeSlot start={date.start_time} end={date.end_time} />
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+          <div className="absolute bottom-0 left-0 right-0 h-12 bg-gradient-to-t from-gray-200"></div>
+        </div>
       </div>
     </div>
   );
