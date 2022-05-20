7f90df354cb3c2a3942c6476c45fc9afc997161d
Reza <rezamajidi.1992@gmail.com>
2022-05-20 19:31:33 +0430

    style: change background color

--- src/components/CompanyHeader.jsx
+++ src/components/CompanyHeader.jsx
@@ -1,6 +1,6 @@
 const CompanyHeader = ({ name, logo, type }) => {
   return (
-    <div className="flex items-center p-4 bg-gray-200 rounded-full">
+    <div className="flex items-center p-4 border border-blue-100 rounded-full bg-blue-50">
       <img className="w-24 h-24 rounded-full" src={logo} alt={`${name} logo`} />
       <figcaption className="ml-4">
         <p className="mb-1 text-xl font-bold text-gray-700">{name}</p>
