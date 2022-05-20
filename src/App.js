a64493797eae82bc1e23fae3fe64104992d823a7
Reza <rezamajidi.1992@gmail.com>
2022-05-20 16:39:54 +0430

    feat: add header & container to app

--- src/App.js
+++ src/App.js
@@ -4,7 +4,20 @@ import { StrictMode } from "react";
 const App = () => {
   return (
     <StrictMode>
-      <div>Hello from React ⚛️</div>
+      <div className="min-h-screen pt-12 bg-gray-200">
+        <div className="container px-4 py-8 mx-auto rounded-md shadow-sm bg-gray-50">
+          <h1 className="inline-block pb-2 mb-8 text-2xl font-bold border-b-4">
+            Time Reservation
+          </h1>
+          <div className="overflow-hidden">
+            <div className="flex flex-row overflow-x-scroll">
+              <section className="flex-shrink-0 w-1/3 border">1</section>
+              <section className="flex-shrink-0 w-1/3 border">2</section>
+              <section className="flex-shrink-0 w-1/3 border">3</section>
+            </div>
+          </div>
+        </div>
+      </div>
     </StrictMode>
   );
 };
