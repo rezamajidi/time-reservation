c5bf2edcf391c5af19f24198dbc69711c214c078
Reza <rezamajidi.1992@gmail.com>
2022-05-21 17:52:14 +0430

    feat: add user local time to prevent any timezone issue

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { render } from "react-dom";
 import { useState, useEffect, StrictMode } from "react";
+import { format } from "date-fns";
 import CompanyColumn from "./components/CompanyColumn.jsx";
 import CompanyContext from "./CompanyContext";
 
 const App = () => {
   const [companies, setCompanies] = useState([]);
+  const [currentLocalTime, setCurrentLocalTime] = useState(new Date());
   const fetchCompanyTimeSlots = async () => {
     const res = await fetch("http://localhost:3000/timeslots").then(
       (response) => response.json()
@@ -13,6 +15,10 @@ const App = () => {
   };
   useEffect(() => {
     fetchCompanyTimeSlots();
+    const timerId = setInterval(() => {
+      setCurrentLocalTime(new Date());
+    }, 1000);
+    return () => clearInterval(timerId);
   }, []);
   return (
     <StrictMode>
@@ -21,10 +27,17 @@ const App = () => {
           <div className="container px-4 py-8 mx-auto rounded-md shadow-sm bg-gray-50">
             <header className="flex items-baseline pb-2 mb-8 border-b-4">
               <h1 className="text-2xl font-bold ">Time Reservation</h1>
-              <p className="ml-2 text-xs text-gray-700">
-                All times are in your local time
-              </p>
-              <p></p>
+              <div className="flex items-center flex-1">
+                <p className="ml-2 text-xs text-gray-700">
+                  All times are in your local time
+                </p>
+                <p className="ml-auto text-xs text-gray-600">
+                  Your local time: {format(currentLocalTime, "HH:mm")}
+                </p>
+                <p className="w-3 h-3 p-px ml-2 bg-gray-100 border border-gray-300 rounded-full">
+                  <span className="block w-full h-full bg-green-200 rounded-full animate-ping"></span>
+                </p>
+              </div>
             </header>
             <div className="overflow-hidden">
               <div className="flex flex-row overflow-x-scroll">
